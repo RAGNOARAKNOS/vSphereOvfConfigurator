4fa11b4ec376ab8a76aef95958f909fd19234331
RAGNOARAKNOS <ragnocorp@googlemail.com>
2018-01-11 00:27:54 +0000

    Basic import framework

--- src/PsImportOvf/PsImportOvf.ps1
+++ src/PsImportOvf/PsImportOvf.ps1
@@ -1,2 +1,32 @@
 # Notes found at https://divyen.wordpress.com/2012/03/11/importingexporting-ova-in-vsphere-using-vmware-ovf-command-line-tool/
-# Notes http://www.vmwarebits.com/content/import-and-export-virtual-machines-command-line-vmwares-ovf-tool
+# Notes http://www.vmwarebits.com/content/import-and-export-virtual-machines-command-line-vmwares-ovf-tool#
+# https://www.vmware.com/support/developer/PowerCLI/PowerCLI651/html/Import-VApp.html .
+
+function Deploy-Ovfs 
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $credentialFileLoc,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $importFileLoc
+    )
+    
+    $creds = Import-Csv $credentialFileLoc;
+    $imports = Import-Csv $importFileLoc;
+
+    foreach($cred in $creds)
+    {
+        Write-Host $cred.DataCentreName
+    }
+
+    foreach($import in $imports)
+    {
+        Write-Host "Deploying " $import.DestinationAppName "to" $import.DestinationDcName
+    
+        #Connect-VIServer -Server 
+    }
+}
---
+++ src/PsSetOvfProps/PsSetOvfProps.ps1
@@ -0,0 +1,20 @@
+function Set-VmOvfProps  
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $credentialFileLoc
+    )
+    
+}
+
+function Set-AppOvfProps
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $credentialFileLoc
+    )
+}
