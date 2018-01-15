602577d6b119baa667f70a3ad2a70b912019eba2
RAGNOARAKNOS <ragnocorp@googlemail.com>
2018-01-15 23:01:07 +0000

    Added a run me file to make running the scripts slightly more user friendly

---
+++ src/runMe.ps1
@@ -0,0 +1,6 @@
+. .\PsBuildCsvs\PsBuildCsvs.ps1
+. .\PsImportOvf\PsImportOvf.ps1
+. .\PsSetOvfProps\PsSetOvfProps.ps1
+#. .\PsExportOvf\PsExportOvf.ps1
+
+ExportWSToCSV -excelFileName "TestDataWorkbook-1" -csvLoc "C:\Users\LBowman\Source\Repos\vSphereOvfConfigurator\tests\Examples\"
