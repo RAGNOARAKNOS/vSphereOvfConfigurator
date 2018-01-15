0536008cbde744599f4ef6fc1445f791749c8c3c
RAGNOARAKNOS <ragnocorp@googlemail.com>
2018-01-15 23:01:35 +0000

    Pulled the CSV extractor to the run me

--- src/PsBuildCsvs/PsBuildCsvs.ps1
+++ src/PsBuildCsvs/PsBuildCsvs.ps1
@@ -14,6 +14,4 @@ Function ExportWSToCSV ($excelFileName, $csvLoc)
     $E.Quit()
 }
 
-ExportWSToCSV -excelFileName "TestDataWorkbook-1" -csvLoc "C:\Users\LBowman\Source\Repos\vSphereOvfConfigurator\tests\Examples\"
-
 # Based from https://www.mssqltips.com/sqlservertip/3223/extract-and-convert-all-excel-worksheets-into-csv-files-using-powershell/
